fe0dfd115978d83a72ea0356c47d426062097f14
cherigra <nikitakrasovskij5@gmail.com>
2023-05-16 22:11:38 +0300

    fic(prod): rename prod

--- eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/example/eurekaserver/EurekaServerApplication.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package java.com.example.eurekaserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- eureka-server/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/com/example/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.eurekaserver;
+package java.com.example.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
